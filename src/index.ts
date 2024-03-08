ba8cac0e4ef52bfb0dc447618a769b0f70459b00
Egorke Ovie <138992258+freshovie@users.noreply.github.com>
2024-03-08 08:00:21 +0100

    added

--- src/index.ts
+++ src/index.ts
@@ -96,14 +96,135 @@
 // } = { id: 1, name: "", isGold: true};
 // employee.name = "cyril";
 
-let employee: {
-    id: number,
-    name: string,
-    retire: (date: Date) => void
-} = {
-    id: 1,
-    name: 'Cyril',
-    retire: (date:Date) => {
-        console.log(date)
+// let employee: {
+//     id: number,
+//     name: string,
+//     retire: (date: Date) => void
+// } = {
+//     id: 1,
+//     name: 'Cyril',
+//     retire: (date: Date) => {
+//         console.log(date)
+//     }
+// };
+
+//type aliases
+// let employee: 
+//     type Employee = {
+//     id: 1,
+//     name: 'Ovie',
+//     retire: (date: Date) => void
+// } = {
+//     id: 1,
+//     name: 'Cyril',
+//     retire: (date: Date) => {
+//         console.log(date)
+//     }
+// };
+
+class Account {
+    readonly id: number;
+    owner: string;
+    balance: number;
+    nickname?: string | null;
+
+    constructor(id: number, owner: string, balance: number, nickname?: string | null) {
+        this.id = id;
+        this.owner = owner;
+        this.balance = balance;
+        this.nickname = nickname;
     }
-};
+
+    deposit(amount: number) {
+        if (amount <= 0)
+        throw new Error('Invalid amount');
+        this.balance += amount;
+    }
+        
+}
+
+let account = new Account(1, "Cyril", 200, null);
+account.deposit(500);
+console.log(account);
+
+
+class Person {
+    constructor(public firstName: string, public lastName: string) {
+    }
+
+  get fullName() {
+    return this.firstName + ' ' + this.lastName;
+  }
+  // add another method called walk()
+  walk() {
+    console.log('Walking')
+  }
+}
+
+class Student extends Person {
+    // the student class can have it own property that's is not present in the parent class
+    constructor(public studentId: number, firstName: string, lastName: string) {
+    // since the student class is extending the parent, typescript will prompt us to use the super() method 
+      super(firstName, lastName)
+    }
+    // we can also add another method to the student class that's not present in the parent
+    takeTest() {
+      console.log('Taking a test');
+    }
+}
+
+class Teacher extends Person {
+    // the student class can have it own property that's is not present in the parent class
+    constructor(public teacherId: number, firstName: string, lastName: string, public qualification: string) {
+    // since the student class is extending the parent, typescript will prompt us to use the super() method 
+      super(firstName, lastName)
+    }
+    // we can also add another method to the student class that's not present in the parent
+    giveTest() {
+      console.log('Giving a test');
+    }
+
+    get teacherQualify() {
+        return this.firstName + ' ' + this.lastName + ' ' + this.qualification
+    }
+}
+
+let student = new Student(1, 'Cyril', 'Okeleke');
+let teacher = new Teacher(10, 'Felix', 'Okeleke', 'BSC');
+student.takeTest();
+console.log(student);
+
+class Ride {
+    private static _activeRides: number = 5;
+
+    start() { Ride._activeRides++};
+    stop() { Ride._activeRides--};
+
+    get ride() {
+        return Ride._activeRides
+    }
+}
+
+let ride1 = new Ride()
+ride1.start();
+
+let ride2 = new Ride()
+ride2.stop();
+
+let ride3 = new Ride()
+ride3.stop();
+
+let ride4 = new Ride()
+ride4.start();
+
+let ride5 = new Ride()
+ride5.start();
+
+let ride6 = new Ride()
+ride6.start();
+
+let ride7 = new Ride()
+ride7.start();
+
+console.log(ride1.ride);
+
