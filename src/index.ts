3358fbb74feb60f47f9e7cfe57c67eeff5a04a12
Egorke Ovie <138992258+freshovie@users.noreply.github.com>
2024-03-14 10:17:32 +0100

    more t

--- src/index.ts
+++ src/index.ts
@@ -15,9 +15,7 @@
 //     console.log(`${name}`)
 // }
 
-
 // let user: [number, string, boolean, number] = [1, 'cyril', true, 2 ]
-
 
 // const small = 1;
 // const medium = 2;
@@ -71,7 +69,7 @@
 //         return 0;
 //     }
 // }
-        
+
 // function calculateTax(income: number, taxYear: number): number {
 //     if (income < 50_000)
 //     return income * 1.2;
@@ -109,7 +107,7 @@
 // };
 
 //type aliases
-// let employee: 
+// let employee:
 //     type Employee = {
 //     id: 1,
 //     name: 'Ovie',
@@ -123,30 +121,82 @@
 // };
 
 class Account {
-    readonly id: number;
-    owner: string;
-    balance: number;
-    nickname?: string | null;
+  readonly id: number;
+  owner: string;
+  balance: number;
+  nickname?: string | null;
 
-    constructor(id: number, owner: string, balance: number, nickname?: string | null) {
-        this.id = id;
-        this.owner = owner;
-        this.balance = balance;
-        this.nickname = nickname;
-    }
+  constructor(
+    id: number,
+    owner: string,
+    balance: number,
+    nickname?: string | null
+  ) {
+    this.id = id;
+    this.owner = owner;
+    this.balance = balance;
+    this.nickname = nickname;
+  }
 
-    deposit(amount: number) {
-        if (amount <= 0)
-        throw new Error('Invalid amount');
-        this.balance += amount;
-    }
-        
+  deposit(amount: number) {
+    if (amount <= 0) throw new Error("Invalid amount");
+    this.balance += amount;
+  }
 }
 
 let account = new Account(1, "Cyril", 200, null);
 account.deposit(500);
 console.log(account);
 
+// class Person {
+//   constructor(public firstName: string, public lastName: string) {}
+
+//   get fullName() {
+//     return this.firstName + " " + this.lastName;
+//   }
+//   // add another method called walk()
+//   walk() {
+//     console.log("Walking");
+//   }
+// }
+
+// class Student extends Person {
+//   // the student class can have it own property that's is not present in the parent class
+//   constructor(public studentId: number, firstName: string, lastName: string) {
+//     // since the student class is extending the parent, typescript will prompt us to use the super() method
+//     super(firstName, lastName);
+//   }
+//   // we can also add another method to the student class that's not present in the parent
+//   takeTest() {
+//     console.log("Taking a test");
+//   }
+// }
+
+// class Teacher extends Person {
+//   // the student class can have it own property that's is not present in the parent class
+//   constructor(
+//     public teacherId: number,
+//     firstName: string,
+//     lastName: string,
+//     public qualification: string
+//   ) {
+//     // since the student class is extending the parent, typescript will prompt us to use the super() method
+//     super(firstName, lastName);
+//   }
+//   // we can also add another method to the student class that's not present in the parent
+//   giveTest() {
+//     console.log("Giving a test");
+//   }
+
+//   get teacherQualify() {
+//     return this.firstName + " " + this.lastName + " " + this.qualification;
+//   }
+// }
+
+// let student = new Student(1, "Cyril", "Okeleke");
+// let teacher = new Teacher(10, "Felix", "Okeleke", "BSC");
+// student.takeTest();
+// console.log(student);
 
 class Person {
     constructor(public firstName: string, public lastName: string) {
@@ -156,75 +206,177 @@ class Person {
     return this.firstName + ' ' + this.lastName;
   }
   // add another method called walk()
-  walk() {
+  protected walk() {
     console.log('Walking')
+  }
+
+  protected balance() {
+    console.log('my balance')
   }
 }
 
+// creating student class extending Person
 class Student extends Person {
-    // the student class can have it own property that's is not present in the parent class
     constructor(public studentId: number, firstName: string, lastName: string) {
-    // since the student class is extending the parent, typescript will prompt us to use the super() method 
       super(firstName, lastName)
     }
-    // we can also add another method to the student class that's not present in the parent
     takeTest() {
+        this.balance()
       console.log('Taking a test');
     }
 }
 
-class Teacher extends Person {
-    // the student class can have it own property that's is not present in the parent class
-    constructor(public teacherId: number, firstName: string, lastName: string, public qualification: string) {
-    // since the student class is extending the parent, typescript will prompt us to use the super() method 
-      super(firstName, lastName)
-    }
-    // we can also add another method to the student class that's not present in the parent
-    giveTest() {
-      console.log('Giving a test');
-    }
+// class Teacher extends Person {
+//     constructor(public teacherId: number, firstName: string, lastName: string, public qualification: string) {
+//       super(firstName, lastName)
+//     }
+//     giveTest() {
+//       console.log('Giving a test');
+//     }
 
-    get teacherQualify() {
-        return this.firstName + ' ' + this.lastName + ' ' + this.qualification
+//     get teacherQualify() {
+//         return this.firstName + ' ' + this.lastName + ' ' + this.qualification
+//     }
+// }
+
+class Teacher extends Person {
+
+    override get fullName() {
+        return 'Professor' + ' ' + super.fullName;
+    }
+      
+    giveTest() {
+        console.log('Giving a test');
     }
 }
 
-let student = new Student(1, 'Cyril', 'Okeleke');
-let teacher = new Teacher(10, 'Felix', 'Okeleke', 'BSC');
-student.takeTest();
-console.log(student);
+class Principal extends Person {
+    override get fullName() {
+      return 'Principal' + ' ' + super.fullName;
+    }
+}
+    
+
+function printNames(people: Person[]) {
+    for (let person of people)
+      console.log(person.fullName);
+}
+
+printNames([
+    new Student(1, 'Cyril', 'Okeleke'),
+    new Teacher('Ovie', 'Raji'),
+    new Principal('Evans', 'Chidi')
+])
+    
+    
+let teacher = new Teacher('Cyril', 'Ovie');
+let student = new Student(1, 'John', 'Ovie');
+
+
+
+
+// teacher.giveTest();
+
+// console.log(teacher.fullName)
+// console.log(student.fullName)
+
+
+console.log(Math.floor(Math.random() * 100));
 
 class Ride {
-    private static _activeRides: number = 5;
+  private static _activeRides: number = 5;
 
-    start() { Ride._activeRides++};
-    stop() { Ride._activeRides--};
+  start() {
+    Ride._activeRides++;
+  }
+  stop() {
+    Ride._activeRides--;
+  }
 
-    get ride() {
-        return Ride._activeRides
-    }
+  get ride() {
+    return Ride._activeRides;
+  }
 }
 
-let ride1 = new Ride()
+let ride1 = new Ride();
 ride1.start();
 
-let ride2 = new Ride()
+let ride2 = new Ride();
 ride2.stop();
 
-let ride3 = new Ride()
+let ride3 = new Ride();
 ride3.stop();
 
-let ride4 = new Ride()
+let ride4 = new Ride();
 ride4.start();
 
-let ride5 = new Ride()
+let ride5 = new Ride();
 ride5.start();
 
-let ride6 = new Ride()
+let ride6 = new Ride();
 ride6.start();
 
-let ride7 = new Ride()
+let ride7 = new Ride();
 ride7.start();
 
 console.log(ride1.ride);
 
+class SeatAssignment {
+    [seatNumber: string]: string;
+}
+let seats = new SeatAssignment();
+seats.seatNumber1 = 'Cyril';
+seats.seatNumber2 = 'John';
+console.log(seats);
+
+abstract class Shape {
+    constructor(public color: string) {}
+    abstract render(): void;
+}
+
+class Circle extends Shape {
+    constructor(public radius: number, color: string) {
+      super(color);
+    }
+
+    override render(): void {
+        console.log('Rendering a circle')
+    }
+}
+
+let circle = new Circle(5, 'red');
+circle.render()
+console.log(circle);
+
+
+
+
+// class Ride {
+//     static activeRides: number = 5;
+
+//     start() { Ride.activeRides++};
+//     stop() { Ride.activeRides--}
+// }
+
+// let ride1 = new Ride()
+// ride1.stop();
+
+// let ride2 = new Ride()
+// ride2.stop();
+
+// let ride3 = new Ride()
+// ride3.stop();
+
+// let ride4 = new Ride()
+// ride4.start();
+
+// let ride5 = new Ride()
+// ride5.start();
+
+// let ride6 = new Ride()
+// ride6.start();
+
+// let ride7 = new Ride()
+// ride7.start();
+
+// console.log(Ride.activeRides);
