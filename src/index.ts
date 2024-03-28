bc88737acafa15aa0d1ff128417267aabe6451e8
Egorke Ovie <138992258+freshovie@users.noreply.github.com>
2024-03-28 18:28:54 +0100

    generic

--- src/index.ts
+++ src/index.ts
@@ -479,3 +479,30 @@ class ArrayUtils {
 let numbers = ArrayUtils.wrapInArray(8);
 console.log(numbers[0]);
 
+interface Product {
+  name: string;
+  price: number;
+}
+class Store<T> {
+  protected _objects: T[] = [];
+  add(obj: T): void {
+    this._objects.push(obj);
+  }
+}
+
+class CompressibleStore<T> extends Store<T> {
+  compress() {}
+}
+
+class SearchableStore<T extends { name: string }> extends Store<T> {
+  find(name: string): T | undefined {
+    return this._objects.find((obj) => obj.name === name);
+  }
+}
+
+// class ProductStore extends Store<Product> {
+//     filterByCategory(category: string): Product[] {
+//     return [];
+// }
+
+let store = new CompressibleStore<Product>();
